3087d51b8950acabe6c44ac30127afc3337f5ff5
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-03-23 14:53:40 +0100

    Use new common config() function for setting up the process

--- occo/api/common.py
+++ occo/api/common.py
@@ -19,34 +19,7 @@ import occo.infraprocessor
 import yaml
 
 def init():
-    #
-    ## Find and load main config file
-    #
-    cfg = config.DefaultConfig(dict(cfg=None))
-    cfg.add_argument(name='--cfg', dest='cfg_path', type=util.cfg_file_path)
-    cfg.add_argument('node_def')
-    cfg.parse_args()
+    def setup_args(cfg):
+        cfg.add_argument('node_def')
 
-    if not cfg.cfg_path:
-        possible_locations = util.file_locations('occo.yaml',
-            '.',
-            util.curried(util.rel_to_file, basefile=__file__),
-            util.cfg_file_path)
-
-        cfg.cfg_path = util.path_coalesce(*possible_locations)
-
-    with open(cfg.cfg_path) as f:
-        cfg.configuration = yaml.load(f)
-
-    #
-    ## Setup logging
-    #
-    import os
-    import logging
-    import logging.config
-    logging.config.dictConfig(cfg.configuration['logging'])
-
-    log = logging.getLogger('occo')
-    log.info('Staring up; PID = %d', os.getpid())
-
-    return cfg
+    return config.config(setup_args=setup_args)

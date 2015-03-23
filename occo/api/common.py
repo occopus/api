2f382284a7a850d65db2c871ff5fb55aa201a645
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-03-23 15:29:24 +0100

    Use the new !auto_import feature

--- occo/api/common.py
+++ occo/api/common.py
@@ -9,14 +9,6 @@ Author: adam.visegradi@sztaki.mta.hu
 """
 
 import occo.util.config as config
-import occo.util as util
-import occo.infobroker
-import occo.infobroker.cloud_provider
-import occo.infobroker.uds
-import occo.cloudhandler
-import occo.cloudhandler.backends.boto
-import occo.infraprocessor
-import yaml
 
 def init():
     def setup_args(cfg):

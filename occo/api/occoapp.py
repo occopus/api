4e8c9ed6782f91a7ec5459e42003703ee5ead32b
Adam Visegradi <visegradi.adam@sztaki.mta.hu>
2015-06-26 11:03:57 +0000

    Further fixup: yaml_load_file has moved

--- occo/api/occoapp.py
+++ occo/api/occoapp.py
@@ -37,8 +37,8 @@ def setup(setup_args=None, cfg_path=None):
     modvars['servicecomposer'] = occo_infra['servicecomposer']
 
 def yaml_file(filepath):
-    import occo.util
-    return occo.util.yaml_load_file(filepath)
+    import occo.util.config
+    return occo.util.config.yaml_load_file(filepath)
 
 def killall(infra_id, ip):
     import logging

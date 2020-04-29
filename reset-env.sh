311599d28369b33a6c2cba6dcec91a80fa053f25
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2020-04-29 22:34:47 +0000

    Fix dependencies and pip arguments

--- reset-env.sh
+++ reset-env.sh
@@ -8,7 +8,7 @@ rm -rf "$PDIR"
 
 virtualenv -p python3 "$PDIR"
 source "$PDIR"/bin/activate
-pip install -r requirements_test.txt 
+pip install -r requirements_test.txt --find-links https://pip3.lpds.sztaki.hu/packages --no-index
 
 #cat /etc/grid-security/certificates/*.pem >> $(python -m requests.certs)
 
